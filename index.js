89e2d1a3123ce7aabbadf602285e2712c0f3ec64
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-06-30 21:13:57 +0100

    update database connection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express           = require('express'),
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'process.env.DATABASE_URL',
+        connectionString : 'process.env.DATABASE_URL',
         ssl: true        
     }
 });
